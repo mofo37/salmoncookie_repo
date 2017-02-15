06cbbb44601d649660b770e648c3eb35fd51da26
Morgan Fogarty <mofo37@gmail.com>
2017-02-14 22:27:46 -0800

    add total method to Store constructor

--- sales.js
+++ sales.js
@@ -1,21 +1,30 @@
 'use strict';
 
-function Store (StoreName, minCustomersPerHour, maxCustomersPerHour, aveCookies) {
-  this.name = StoreName;
+function Store (storeName, minCustomersPerHour, maxCustomersPerHour, aveCookies) {
+  this.name = storeName;
   this.hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm','6:00pm','7:00pm', '8:00pm'];
   this.minCustomers = minCustomersPerHour;
   this.maxCustomers = maxCustomersPerHour;
   this.averageCookies = aveCookies;
   this.cookiesPerHour = [];
 }
+
 Store.prototype.calculateCookiesPerHour = function() {
   return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers) * this.averageCookies;
 };
+
 Store.prototype.cookiesPerThisHour = function() {
   for (var hour = 0; hour < this.hours.length; hour++) {
-    var hourlyCookies = this.hours[hour] + ': ' + Math.floor(this.calculateCookiesPerHour()) + ' cookies';
-    this.cookiesPerHour.push(hourlyCookies);
+    this.cookiesPerHour.push(Math.floor(this.calculateCookiesPerHour()));
   }
+};
+
+Store.prototype.totalCookiesPerDay = function() {
+  var total = 0;
+  for (var hour = 0; hour < this.cookiesPerHour.length; hour++){
+    total = total + this.cookiesPerHour[hour];
+  }
+  return total;
 };
 
 var pike = new Store('1st and Pike', 23, 65, 6.3);
@@ -24,18 +33,49 @@ var seattle = new Store('Seattle', 11, 38, 3.7);
 var capitol = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
-function table () {
+function addHeadingsToTable () {
   var hours = ['', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm','6:00pm','7:00pm', '8:00pm', 'Total Cookies'];
-  var hourTable = document.getElementById('tom');
+  var hourTable = document.getElementById('coffeeshops');
   var hourTr = document.createElement('tr');
   hourTable.appendChild(hourTr);
+
   for (var i = 0; i < hours.length; i++) {
     var hourTh = document.createElement('th');
     hourTh.textContent = hours[i];
     hourTr.appendChild(hourTh);
   }
-}; 
-  table();
+};
+addHeadingsToTable();
+
+console.log(pike.cookiesPerHour);
+console.log("***");
+pike.cookiesPerThisHour();
+console.log(pike.cookiesPerHour);
+console.log(pike.totalCookiesPerDay());
+
+
+// function pike () {
+//   var shop = pike;
+//   var hourTable = document.getElementById('coffeeshops');
+//   var HourTr = document.createElement('tr');
+//   hourTable.appendChild(HourTr);
+//   for (var i = 0; i < pike.length; i++) {
+//     var hourTable = document.createElement('td');
+//     hourTh.textContent = shop[i];
+//     hourTable.appendChild(hourTh);
+//   }
+// };
+// pike();
+
+// function seatac
+
+// function seattle
+
+// function capitol
+
+// function alki
+
+
 
 
 
